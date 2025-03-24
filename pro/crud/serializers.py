a37d30d1a546141f38536b713ce63304550e2375
Pradeep2278 <pradeepchinnasamy98@gmail.com>
2025-03-24 14:43:51 +0530

    Update serializers.py

--- pro/crud/serializers.py
+++ pro/crud/serializers.py
@@ -1,7 +1,1 @@
-from rest_framework import serializers
-from .models import pradeep
 
-class pradeepSerializers(serializers.ModelSerializer):
-    class Meta:
-        model=pradeep
-        fields="__all__"
